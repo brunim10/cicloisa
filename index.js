69d6d73c9b28fd2e782fb0ed3d49986f218a12d1
brunim10 <131418015+brunim10@users.noreply.github.com>
2024-07-17 15:25:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('cycleCard').addEventListener('click', function() {
+    window.location.href = 'cycle.html';
+});
+
+document.getElementById('academicCard').addEventListener('click', function() {
+    window.location.href = 'link2.html';
+});
+
+document.getElementById('churchCard').addEventListener('click', function() {
+    window.location.href = 'link3.html';
+});
